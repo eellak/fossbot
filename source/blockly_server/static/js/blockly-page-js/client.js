7d53429351643780dd5e9836ce7eaa18774e1ca9
chronis10 <chronis10@hua.gr>
2022-06-30 16:44:25 +0300

    print python script, catch errors

--- source/blockly_server/static/js/blockly-page-js/client.js
+++ source/blockly_server/static/js/blockly-page-js/client.js
@@ -1,48 +1,50 @@
 //send the code from thw workspace to be run in the robot 
-function runCode(id) {
-  let blockly_code = Blockly.Python.workspaceToCode(Blockly.mainWorkspace);
+async function runCode(id) {
+    let blockly_code = Blockly.Python.workspaceToCode(Blockly.mainWorkspace);
 
-  if (blockly_code == "") {
-    console.log("no code to run");
+    if (blockly_code == "") {
+        console.log("no code to run");
 
-    //show modal 
-    showModalErrorInBlocklyPage("Δεν υπάρχουν μπλοκς στον χώρο εργασίας για να μπορέσει να τρέξει το ρομπότ!")
-    return;
-  }
+        //show modal 
+        showModalErrorInBlocklyPage("Δεν υπάρχουν μπλοκς στον χώρο εργασίας για να μπορέσει να τρέξει το ρομπότ!")
+        return;
+    }
 
-  sendcode(id, blockly_code)
-    .then(response => {
-      if (response.status == 200) {
-        showModalsSuccessInBlocklyPage("Ο κώδικας τρέχει με επιτυχία!");
-      }
-    })
+    try {
+        const sendResultCode = await sendcode(id, blockly_code)
+        if (sendResultCode.status == 200) {
+            showModalsSuccessInBlocklyPage("Ο κώδικας τρέχει με επιτυχία!");
+        }
+    } catch (err) {
+        console.log('Error when sending the blockly code\n', err);
+    }
 
-  let xmlDom = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
-  let xmlText = Blockly.Xml.domToPrettyText(xmlDom);
+    let xmlDom = Blockly.Xml.workspaceToDom(Blockly.mainWorkspace);
+    let xmlText = Blockly.Xml.domToPrettyText(xmlDom);
 
-  saveXml(id, xmlText)
-    .then(response => {
-      if (response.status == 200) {
-        console.log("Ο κώδικας αποθήκευτηκε με επιτυχία!");
+    try {
+        const saveXmlResult = await saveXml(id, xmlText)
 
-        printRealtimePythonScripts(id)
-          .then(response => {
+        if (saveXmlResult.status == 200) {
+            console.log("Ο κώδικας αποθήκευτηκε με επιτυχία!");
+        }
+    } catch (err) {
+        console.log('Error when saving the xml\n', err);
+    }
+
+    printRealtimePythonScripts(id)
+        .then(response => {
             if (response.status == 200) {
-              console.log("Ας εμφανισουμε το python script");
-              console.log(response.script)
+                console.log("Ας εμφανισουμε το python script");
+                console.log(response.data.script)
             }
-          })
-          .catch(err => {
+        })
+        .catch(err => {
             console.log('Error when getting the python script\n', err);
-          })
-      }
-    }) 
-    .catch(err => {
-      console.log('Error when saving project\n', err);
-    })
+        })
 }
 
 //stop the code that was being exeuted in the robot 
 function stop_script() {
-  stopScript();
+    stopScript();
 }

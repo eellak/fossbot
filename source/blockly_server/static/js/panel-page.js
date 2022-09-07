8320b5eb25087fe38b63951082a7350d991a5818
Eleftheria Papageorgiou <eleftheria@crowdpolicy.com>
2022-09-07 20:18:55 +0300

    robot name

--- source/blockly_server/static/js/panel-page.js
+++ source/blockly_server/static/js/panel-page.js
@@ -20,15 +20,15 @@ function loadSettings(data) {
             '<td>' + parameter[0]['name'] + '</td>' +
             '<td>' + parameter[2]['default'] + '</td>' +
             '<td>' +  '</td>' +
-            '</tr>';
-            return 
-        }
-        document.getElementById("body-table-parameters").insertRow(-1).innerHTML =
+            '</tr>'; 
+        } else {
+            document.getElementById("body-table-parameters").insertRow(-1).innerHTML =
             '<tr>' +
             '<td>' + parameter[0]['name'] + '</td>' +
             '<td>' + parameter[2]['default'] + '</td>' +
             '<td>' + '<input type="number" id="' + i + '" value="' + parameter[1]['value'] + '">' + '</td>' +
             '</tr>';
+        } 
     }
 }
 

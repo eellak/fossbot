30083e56a5ff612671784770d04f7165961c5d1b
Eleftheria Papageorgiou <eleftheria@crowdpolicy.com>
2022-09-20 23:48:50 +0300

    time

--- source/basic_image/robot_lib/control.py
+++ source/basic_image/robot_lib/control.py
@@ -1,5 +1,5 @@
 import time
-from datetime import datetime
+import datetime
 
 import math
 #import csv
@@ -485,12 +485,12 @@ class timer():
 	def stop_timer(self):
 		self.start = 0	
 	def start_timer(self):
-		self.start = datetime.now()
+		self.start = datetime.datetime.now()
 	def elapsed(self):
 		if self.start == 0:
 			print("Timer not started")
 		else:
-			dif = datetime.now() - self.start
+			dif = datetime.datetime.now() - self.start
 			print("The elapsed time in sec is {}".format(dif))
 	def get_elapsed(self):
 		if self.start == 0:
@@ -498,7 +498,7 @@ class timer():
 			return 0
 		else:
 			print('self.start: ', self.start)
-			elapsed = datetime.now() - self.start
+			elapsed = datetime.datetime.now() - self.start
 			return elapsed.strftime("%S") #returns the elapsed time in seconds
 
 #General functions

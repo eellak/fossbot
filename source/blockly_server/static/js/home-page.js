8f70af72552f203e1b998678735ed3f484cef5ce
Eleftheria Papageorgiou <eleftheria@crowdpolicy.com>
2022-10-15 16:56:54 +0300

    home page translations

--- source/blockly_server/static/js/home-page.js
+++ source/blockly_server/static/js/home-page.js
@@ -8,7 +8,6 @@ function loadProjects(data) {
     //get the array with the projects
     const projects_array = data.data;
     const current_language = data.language;
-    console.log('lang: ', current_language)
 
     // const rows = document.getElementById("body-table-projects").rows.length;
     // if(rows >0){
@@ -17,43 +16,54 @@ function loadProjects(data) {
     //     }
     //     location.reload();
     // }
-    
+
     for (var i = 0; i < projects_array.length; i++) {
         const project = projects_array[i];
-
-        //add every time the the project name as the last row
-        document.getElementById("body-table-projects").insertRow(-1).innerHTML =
-            '<tr>' +
-            '<td>' + project['title'] +'</td>'+
-            '<td>' + project['info'] +'</td>'+
-            '<td>' + project['project_id'] + '</td>' +
-            '<td> <div id="run-Blockly-Button-container" class="run-Blockly-Button-container">' +
-                        '<div id="run-Blockly-Button-wrap" class="run-Blockly-Button-wrap">' +
-                            '<button onclick="execute_script('+ project['project_id'] +')" type="button" class="run-Blockly" id="open-Blockly">' +
-                                'Εκτέλεση' +
-                            '</button>' +
-                        '</div>' +
-                    '</div>' +
-            '</td>' +
-            '<td> <div id="open-Blockly-Button-container" class="open-Blockly-Button-container">' +
-                '<div id="open-Blockly-Button-wrap" class="open-Blockly-Button-wrap">' +
-                    '<button type="button" class="open-Blockly" id="open-Blockly">' +
-                        '<a href="/blockly?id='+ project['project_id'] +'" id="open-Blockly-href" style="color: white; text-decoration: none;">Επεξεργασία</a>' +
-                    '</button>' +
-                '</div>' +
-            '</div>' +
-            '</td>' +
-            '<td>   <div id="delete-Blockly-Button-container" class="delete-Blockly-Button-container">' +
-                        '<div id="delete-Blockly-Button-wrap" class="delete-Blockly-Button-wrap">' +
-                            '<button onclick="deleteElement(this,'+ project['project_id'] +')" type="button" class="delete-Blockly" id="open-Blockly">' +
-                                '<a id="open-Blockly-href" style="color: white; text-decoration: none;">Διαγραφή</a>' +
-                            '</button>' +
-                        '</div>' +
-                    '</div>' +
-            '</td>' +
-            '</tr>';
+        if (current_language == 'en') {
+            showNewProject(project['title'], project['info'], project['project_id'], "Εκτέλεση", "Επεξεργασία", "Διαγραφή");
+        } else {
+            showNewProject(project['title'], project['info'], project['project_id'], "Execution", "Edit", "Delete");
+        }
     }
 
+}
+
+function showNewProject(title, info, project_id, string_execution, string_edit, string_delete) {
+    //add every time the the project name as the last row
+    document.getElementById("body-table-projects").insertRow(-1).innerHTML =
+    '<tr>' +
+    '<td>' + title + '</td>' +
+    '<td>' + info + '</td>' +
+    '<td>' + project_id + '</td>' +
+    '<td> <div id="run-Blockly-Button-container" class="run-Blockly-Button-container">' +
+    '<div id="run-Blockly-Button-wrap" class="run-Blockly-Button-wrap">' +
+    '<button onclick="execute_script(' + project_id + ')" type="button" class="run-Blockly" id="open-Blockly">' +
+    string_execution +
+    '</button>' +
+    '</div>' +
+    '</div>' +
+    '</td>' +
+    '<td> <div id="open-Blockly-Button-container" class="open-Blockly-Button-container">' +
+    '<div id="open-Blockly-Button-wrap" class="open-Blockly-Button-wrap">' +
+    '<button type="button" class="open-Blockly" id="open-Blockly">' +
+    '<a href="/blockly?id=' + project_id + '" id="open-Blockly-href" style="color: white; text-decoration: none;">' + 
+    string_edit +
+    '</a>' +
+    '</button>' +
+    '</div>' +
+    '</div>' +
+    '</td>' +
+    '<td>   <div id="delete-Blockly-Button-container" class="delete-Blockly-Button-container">' +
+    '<div id="delete-Blockly-Button-wrap" class="delete-Blockly-Button-wrap">' +
+    '<button onclick="deleteElement(this,' + project_id + ')" type="button" class="delete-Blockly" id="open-Blockly">' +
+    '<a id="open-Blockly-href" style="color: white; text-decoration: none;">' + 
+    string_delete + 
+    '</a>' +
+    '</button>' +
+    '</div>' +
+    '</div>' +
+    '</td>' +
+    '</tr>';
 }
 
 function createNewProject() {
@@ -89,18 +99,18 @@ async function getDescription() {
         //empty the input value 
         document.getElementById("project-description-text").value = " ";
 
-        const result = await newProject(new_project_title,new_project_description)
+        const result = await newProject(new_project_title, new_project_description)
         console.log('result is ', result)
-        window.location.replace('/blockly?id='+ result.project_id)
+        window.location.replace('/blockly?id=' + result.project_id)
     }
 }
 
-async function deleteElement(el,id) {
-    var tbl = el.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;    
-    var row = el.parentNode.parentNode.parentNode.parentNode.rowIndex;    
+async function deleteElement(el, id) {
+    var tbl = el.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+    var row = el.parentNode.parentNode.parentNode.parentNode.rowIndex;
 
     const result = await deleteProject(id)
-    if(result.status == '200') {
+    if (result.status == '200') {
         tbl.deleteRow(row);
         location.reload();
     }
@@ -125,7 +135,7 @@ function stop_script() {
 function showRobotName() {
     var hostname = window.location.hostname;
     let array = hostname.split("-").join(" ").split(".").join(" ");
-    array =  array.split(" ", 2)
+    array = array.split(" ", 2)
     if (array[0] && array[1]) {
         document.getElementById("robot-name").innerHTML = array[0] + " " + array[1]
     } else {
